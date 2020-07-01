175a8db00f469fdcc8b09a3cc381beeea2bb5fce
Bibek Dahal <bibekdahal.bd16@gmail.com>
2020-07-01 16:51:39 -0500

    Use default wallpaper when network request fails

--- splash-me.py
+++ splash-me.py
@@ -6,10 +6,14 @@ from os import system, path
 
 directory = path.dirname(path.abspath(__file__))
 image_path = path.join(directory, 'wallpaper')
+system(f'feh --bg-scale {image_path}')
 
-url = 'https://source.unsplash.com/1920x1080/?nature,water'
-r = requests.get(url=url, stream=True)
-with open(image_path, 'wb') as out_file:
-    shutil.copyfileobj(r.raw, out_file)
+try:
+    url = 'https://source.unsplash.com/1920x1080/?nature'
+    r = requests.get(url=url, stream=True)
+    with open(image_path, 'wb') as out_file:
+        shutil.copyfileobj(r.raw, out_file)
+except Exception:
+    pass
 
 system(f'feh --bg-scale {image_path}')
